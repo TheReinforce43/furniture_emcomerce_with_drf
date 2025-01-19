96414cda482db51a7adcd532e45f245770c44415
Md. Rahim Uddin <rahimuddinduet90@gmail.com>
2025-01-19 18:38:38 +0600

    added ecommerce backend file

--- e_commerce_backend/settings.py
+++ e_commerce_backend/settings.py
@@ -42,7 +42,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['e-commerce-backend-st6f.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
